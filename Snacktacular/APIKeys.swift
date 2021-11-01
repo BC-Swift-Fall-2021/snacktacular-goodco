dc65f264eb6ccecd78e16cedb7bacd928e5e178c
Connor Goodman <goodmaco@bc.edu>
2021-11-01 16:15:18 -0400

    Completed Week 10 Work (8.1-8.8)

---
+++ Snacktacular/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  Snacktacular
+//
+//  Created by Connor Goodman on 11/1/21.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyAl0ofOZcVIXYB0L3AOCJ4Obxfoxiz_95Y"
+}
---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Connor Goodman on 10/31/21.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    
+}
